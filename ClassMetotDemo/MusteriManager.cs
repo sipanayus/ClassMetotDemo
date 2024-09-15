58e65e84809df2f3360644f35b0c4e78e4356716
RED <RED@SIPAN>
2024-09-15 18:34:38 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+       //public Ekle(Musteri musteri)
+       // {
+
+       //     Console.WriteLine( "tebrikler artık müşterisiniz : " +musteri.Adi);
+       // }
+
+        //public Sil(Musteri musteri)
+        //{
+        //    Console.WriteLine( "aboneliğiniz silindi : " +musteri.Id);
+        //}
+
+     
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(  "tebrikler artık müşterisiniz : " +musteri.Adi);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(  "aboneliğiniz silindi : " +musteri.Adi);
+        }
+
+        //public void Listele(Musteri musteri)
+        //{
+        //    Console.WriteLine(" Listelendi");
+        //} Listeleme metodu olmadı tekrar  yap
+
+    }
+
+
+
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassMetotDemo;
+
+Console.WriteLine("Hello, World!");
+
+
+//string adi = "ali";
+//string soyadi = "ak";
+//int id = 1;
+
+//string[] musteriler = new string[] { };
+
+
+Musteri musteri1 = new Musteri();
+musteri1.Adi = "ali";
+musteri1.Soyadi = "al";
+musteri1.Id = 1;
+
+
+Musteri musteri2 = new Musteri();
+musteri2.Adi = "sipan";
+musteri2.Soyadi = "ay";
+musteri2.Id = 2;
+
+Musteri musteri3 = new Musteri();
+musteri3.Adi = "ayşe";
+musteri3.Soyadi = "gel";
+musteri3.Id = 3;
+
+Musteri[] musteriler = new Musteri[] {musteri1, musteri2, musteri3};
+
+foreach (Musteri musteri in musteriler)
+{
+    Console.WriteLine(musteri.Adi );
+    Console.WriteLine(  musteri.Soyadi);
+    Console.WriteLine(  musteri.Id);
+}
+
+Console.WriteLine(  "---------metotlar bitti--------");
+
+
+MusteriManager musteriManager = new MusteriManager();
+
+musteriManager.Ekle(musteri2);
+musteriManager.Ekle(musteri1);
+musteriManager.Ekle(musteri3);
+
+
+musteriManager.Sil(musteri3);
+
+//musteriManager.Listele(musteri1 ); listele olmadı
+
+
+
+
+
+
